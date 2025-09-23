0e6be2feee709644ac351f9ef94d293e038a7719
Назар Коробчук <nazarkorobchuk08@gmail.com>
2025-09-23 17:18:16 +0300

    Refactor solution structure and add new projects
    Significant restructuring of the solution:
    - Added new projects: `Domain`, `AppBLL`, `Infrastructure`, `DAL`, and `PersistanceContext`, all targeting .NET 8.0.
    - Removed `WeatherForecastController` and `WeatherForecast` classes.
    - Updated `GooseTapBackend.csproj` with new dependencies and project references.
    - Added `UserController` with a `GetName` endpoint and `UserService` placeholder in `AppBLL`.
    - Introduced `ApplicationUser` class in `Infrastructure.Identity` for extended ASP.NET Core Identity functionality.
    - Added `EmailSettings` class for SMTP configuration in `Infrastructure.Configuration`.
    - Updated paths, configurations, and NuGet settings to reflect a new directory structure and user environment (`Nazar`).
    - Performed general cleanup, removed outdated files, and updated binary files.

---
+++ Backend/GooseTapBackend/GooseTapBackend/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GooseTapBackend.Controllers
+{
+    [Route("api/userdata")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult<string> GetName(int userId)
+        {
+            var str = "Nazar";
+            return  Unauthorized(str);
+        }
+    }
+}
